f85365369184ac2d0e85f77480835212df8aefa1
rmashhadi <rme1383@gmail.com>
2020-03-07 09:04:19 +0330

    Updates

---
+++ precache-manifest.906f32f68a611e58da01c50281434026.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d73318f0d9972ce6af4644911243512",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "74a733a1c6fd954f4747",
+    "url": "/task/static/css/main.b136000c.chunk.css"
+  },
+  {
+    "revision": "ad3fc5927a48f7a12a46",
+    "url": "/task/static/js/2.878701cd.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/task/static/js/2.878701cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74a733a1c6fd954f4747",
+    "url": "/task/static/js/main.46549270.chunk.js"
+  },
+  {
+    "revision": "ac38d49ab11df9b7dca9",
+    "url": "/task/static/js/runtime-main.cca6b474.js"
+  },
+  {
+    "revision": "626be8b3c3039d48d99e128d2a16f049",
+    "url": "/task/static/media/background.626be8b3.png"
+  },
+  {
+    "revision": "7529d9dbfdd4d0c47bdf745cbf9c6889",
+    "url": "/task/static/media/background.7529d9db.png"
+  },
+  {
+    "revision": "372ac3f6809ac74e4cec27d1ec59eb47",
+    "url": "/task/static/media/userlock.372ac3f6.png"
+  }
+]);
